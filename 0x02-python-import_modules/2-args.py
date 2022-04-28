433d7545ba88722a654a0f811881b3c99bb0f24a
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-04-28 08:12:21 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+argCount = len(argv)
+if argCount == 1:
+    print(f"0 arguments.")
+if argCount > 1:
+    if argCount == 2:
+        print(f"1 argument:")
+    else:
+        print(f"{argCount - 1} arguments:")
+    for i in range(1, argCount):
+        print(f"{i}: {argv[i]}")
