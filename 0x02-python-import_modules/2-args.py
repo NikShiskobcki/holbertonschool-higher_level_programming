fffede202c6fbf7cc0a68f8c63cd3b739c990d24
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-04-28 09:02:08 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-argCount = len(argv)
-if argCount == 1:
-    print(f"0 arguments.")
-if argCount > 1:
-    if argCount == 2:
-        print(f"1 argument:")
-    else:
-        print(f"{argCount - 1} arguments:")
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argCount = len(argv)
+    if argCount == 1:
+        print(f"0 arguments.")
+    if argCount > 1:
+        if argCount == 2:
+            print(f"1 argument:")
+        else:
+            print(f"{argCount - 1} arguments:")
     for i in range(1, argCount):
         print(f"{i}: {argv[i]}")
