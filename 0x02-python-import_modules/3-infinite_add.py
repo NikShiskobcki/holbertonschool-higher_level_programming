88bb3892d214c68e3d111db5571dbed7631a6f7b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-04-28 09:04:05 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-argv = sys.argv
-argCount = len(argv)
-res = 0
-for i in range(1, argCount):
-    val = int(argv[i])
-    res += val
-print(f"{res}")
+    import sys
+    argv = sys.argv
+    argCount = len(argv)
+    res = 0
+    for i in range(1, argCount):
+        val = int(argv[i])
+        res += val
+    print(f"{res}")
