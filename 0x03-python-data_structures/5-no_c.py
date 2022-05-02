f5e4dc1cd5231327e52dbb4d96d303282eed53b7
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-02 09:50:14 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     aux = ''
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
-             aux += my_str[i]
+            aux += my_string[i]
     return aux
