6c0b2def8e4f26b82a155b383ae013d386a70380
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-02 10:26:31 -0700

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    if length == 0:
+        first_chr = None
+    else:
+        first_chr = sentence[0]
+    data = (lenght, first_chr)
+    return data
