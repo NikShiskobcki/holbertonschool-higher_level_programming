dfed63145de2bc14f4a69f00aa2b833f84bb57a9
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-02 09:37:12 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    aux = my_list
+    aux = list(my_list)
     if (idx < 0) or (idx >= len(my_list)):
         return aux
     aux[idx] = element
