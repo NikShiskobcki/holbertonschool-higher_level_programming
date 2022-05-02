ab3aea8588c0ad5c05d39d7f5d586e69ebb5ad57
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-02 10:30:01 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lenght = len(sentence)
+    length = len(sentence)
     if length == 0:
         first_chr = None
     else:
         first_chr = sentence[0]
-    data = (lenght, first_chr)
+    data = (length, first_chr)
     return data
