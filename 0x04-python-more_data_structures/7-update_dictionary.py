485afd797a8e11ddae6182ed92178a24169c136d
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-04 09:55:17 -0700

    task /

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()    
-    new_dict[key] = value
-    return(new_dict)
+def update_dictionary(a_dictionary, key, value):    
+    a_dictionary[key] = value
+    return(a_dictionary)
