01acefb617376046273ca9abb570cea579cdfacc
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-16 07:41:58 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for list in range(x):
+            print(f"{my_list[list]}", end="")
+        print("")
+    except:
+        print("")
+        return list
+    return x
+
