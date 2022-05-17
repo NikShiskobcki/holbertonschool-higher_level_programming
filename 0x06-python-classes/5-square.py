7d14a76844b2895e5ae2fa9e734dcc823054720a
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-17 09:56:11 -0700

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """init size"""
+        self.size = size
+
+    def area(self):
+        """current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieves size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints square to stdout"""
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                for i in range(self.size):
+                    print("#", end=" ")
+                print("")
+		
