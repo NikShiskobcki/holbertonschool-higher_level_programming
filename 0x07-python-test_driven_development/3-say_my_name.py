fcf874872601172c56cc5be5d1a47ea111d99a79
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-20 10:55:39 -0700

    task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,13 +9,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    if (first_name) and (last_name):
-        print(f"My name is {first_name} {last_name}")
-    else:
-        if (first_name) and (not last_name):
-            print(f"My name is {first_name} ")
-        else:
-            if (first_name == "") and (last_name):
-                print(f"My name is {last_name}")
-    if first_name == "" and last_name == "":
-        print(f"My name is    ")
+    print(f"My name is {first_name} {last_name}")
