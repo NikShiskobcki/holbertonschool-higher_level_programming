987af87befe65876bcfaad34072e6475bfd24166
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-30 11:07:50 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class basegeometry"""
+
+
+class BaseGeometry:
+    """class baseGeometry"""
+    
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
