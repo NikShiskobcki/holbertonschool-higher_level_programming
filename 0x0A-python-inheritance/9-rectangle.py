2ad62d5a8e5f93a0b42d4d6cc4f89abc1c0df768
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-30 14:31:59 -0700

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,9 +4,10 @@
 
 class BaseGeometry:
     """class baseGeometry"""
-    
+
     def area(self):
         """area"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -16,11 +17,13 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """class rectangle"""
 
     def __init__(self, width, height):
         """init"""
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
