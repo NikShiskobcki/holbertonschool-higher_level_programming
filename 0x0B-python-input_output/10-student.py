c3893c4a2d74f825404311a3de88b1878d86c3b6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-31 09:48:48 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""function"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dict representation"""
+        if tyoe(attrs) is list:
+            res = {}
+            for i in attrs:
+                if i in self.__dict:
+                    res[i] = self.dict[i]
+        else:
+            return dict(self.__dict__)
