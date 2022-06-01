d4d91f848fc6640147368292244f9755bd0b046c
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-06-01 06:00:41 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,4 +5,13 @@
 def pascal_triangle(n):
     """pascal triangle"""
     lst = []
-    return lst
+    lstAux = []
+    if n <= 0:
+        return []
+    for x in range(n):
+        lst = [1]
+        if x > 0:
+            for i in range(x):
+                lst.append(sum(lstAux[-1][i:i+2]))
+        lstAux.append(lst)
+    return (lstAux)
