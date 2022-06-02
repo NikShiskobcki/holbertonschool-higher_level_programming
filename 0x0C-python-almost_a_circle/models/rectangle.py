6bc693c9d87e9a6c478117bd93829eb186d3fef8
root <nicolleshiskobcki@gmail.com>
2022-06-02 11:23:45 -0700

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""function"""
+from .base import Base
+
+
+class Rectangle(Base):
+    """rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        else:
+            if name == "x" or name == "y":
+                if value < 0:
+                    raise TypeError("{} must be >= 0".format(name))
+            elif name == "width" or name == "height":
+                if value <= 0:
+                    raise ValueError("{} must be > 0".format(name))
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """"y setter"""
+        self.integer_validator("y", value)
+        self.__y = value
+
