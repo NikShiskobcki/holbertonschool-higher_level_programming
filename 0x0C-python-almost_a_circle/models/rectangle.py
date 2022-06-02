3ab8aa9c7ce2b88caa353f0920c261f3526929c9
root <nicolleshiskobcki@gmail.com>
2022-06-02 12:26:23 -0700

    took care of x and y task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints rectangle"""
+        for y in range(self.__y):
+            print("")
         for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print("")
