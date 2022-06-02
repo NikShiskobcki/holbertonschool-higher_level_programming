badb9cb96c215dfada51a534db38ab3b2a199e2b
root <nicolleshiskobcki@gmail.com>
2022-06-02 12:15:47 -0700

    added display task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,10 @@ class Rectangle(Base):
     def area(self):
         """area"""
         return self.__width * self.__height
+
+    def display(self):
+        """prints rectangle"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
