217437b1cb82347919a26bf5476ee12e687b3c56
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-06 11:11:59 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates server user with privileges
-CREATE USER  IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
