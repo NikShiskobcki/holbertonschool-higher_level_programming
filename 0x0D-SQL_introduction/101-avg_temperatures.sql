3eb7f784aef6a5e120ed8809d839bac44e54615f
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-05 17:08:33 -0700

    task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- average temperatures
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY avg_temp DESC
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
