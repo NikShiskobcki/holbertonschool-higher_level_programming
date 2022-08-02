76e75d395b2d0221f4c04be7eafa59b9c47636d6
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-02 11:03:21 -0700

    task (

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const aux = [];
-  for (let i = list.lenght; i > 0; i--) {
+  for (let i = list.lenght - 1; i >= 0; i--) {
     aux.push(list[i]);
   }
   return aux;
