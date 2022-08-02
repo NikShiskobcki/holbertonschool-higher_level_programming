f1f81e838a6191d5c3616da73365a7049b7be6ab
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-02 10:57:24 -0700

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      occurences = occurences++;
+      occurences = occurences + 1;
     }
   }
   return occurences;
