09fda4fbb2814a314253c080bfccafb2f1b3dd7e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-06 12:43:26 -0700

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
-
-    def __init__(self, name):
-        self.name = name
