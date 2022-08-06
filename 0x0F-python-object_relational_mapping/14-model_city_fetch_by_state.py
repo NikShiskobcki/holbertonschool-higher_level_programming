8a89601eb6510adc890de7bfae259478f5ca276e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-06 14:07:42 -0700

    task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""update state module"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    session = Session(bind=engine)
+    for stat, cit in session.query(State, City).filter(
+            State.id == City.state_id):
+                print("{}: ({}) {}".format(stat.name, cit.id, cit.name))
