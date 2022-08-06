25a32c0ea8561b773c8d765af77972e5f6dc22f3
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-06 13:52:48 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module"""
+from sys import argv
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine(
+            f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    session = Session(bind=engine)
+    ans = session.query(State).filter(State.name == argv[4]).\
+            order_by(State.id).first()
+    if (ans is not None):
+        print(ans.id)
+    else:
+        print("Not Found")
+    session.close()
