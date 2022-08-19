f71f37a0bfb86b815594d460f465685149549a0a
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-19 11:05:21 -0700

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes github credentials and displays id using github api"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    header = {'Authorization': "Bearer {}".format(argv[2])}
+    req = requests.get("https://api.github.com/users/{}".format(argv[1]),
+                        header)
+    try:
+        print(req.json()['id'])
+    except Exception:
+        print(None)
