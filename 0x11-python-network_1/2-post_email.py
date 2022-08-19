928a9f18601be671549fd817ea1bbf00036db53d
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-19 10:52:49 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """takes a url and email and sends a post request"""
-import sys
+from sys import argv
 from urllib import request, parse
 
 if __name__ == "__main__":
-    dta = parse.urlencode({'email': sys.argv[2]}).encode()
-    req = request.Request(sys.argv[1], dta=dta)
+    dta = parse.urlencode({'email': argv[2]}).encode()
+    req = request.Request(argv[1], dta=dta)
     with request.urlopen(req) as response:
         page = response.read()
     print(page.decode("utf-8"))
